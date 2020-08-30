4319f792fa0105579bedf900df02ce84326820b8
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2020-08-31 07:25:18 +0900

    Add files via upload

---
+++ day11/springbootjdbc/src/main/java/myepark/ebay/springbootjdbc/SpringbootjdbcApplication.java
@@ -0,0 +1,13 @@
+package myepark.ebay.springbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootjdbcApplication.class, args);
+    }
+
+}
