b93f815044f6a378fdd0a3396cf72a03303138cd
Azderica <mh97888@gmail.com>
2020-08-30 14:30:07 +0900

    day10

---
+++ day10/springcorsclient/src/test/java/myepark/ebay/springcorsclient/SpringcorsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package myepark.ebay.springcorsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
