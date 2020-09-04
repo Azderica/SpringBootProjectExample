2b118ea1da4fcfc9cfd5a3fc2f9e50cdd0fa7031
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2020-09-04 11:20:55 +0900

    Add files via upload

---
+++ project14/springbootmongo/src/main/java/myepark/ebaykorea/springbootmongo/SpringbootmongoApplication.java
@@ -0,0 +1,33 @@
+package myepark.ebaykorea.springbootmongo;
+
+import myepark.ebaykorea.springbootmongo.account.Account;
+import myepark.ebaykorea.springbootmongo.account.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@SpringBootApplication
+public class SpringbootmongoApplication {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmongoApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(){
+        return args -> {
+            Account account = new Account();
+            account.setEmail("myepark@ebaykorea");
+            account.setUsername("myepark");
+            accountRepository.insert(account);
+
+            System.out.println("finished");
+        };
+    }
+}
---
+++ project14/springbootmongo/src/main/java/myepark/ebaykorea/springbootmongo/account/AccountRepository.java
@@ -0,0 +1,9 @@
+package myepark.ebaykorea.springbootmongo.account;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+    Optional<Account> findByEmail(String email);
+}
