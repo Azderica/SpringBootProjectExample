bea2f861c23320786ce25e5ba7f94223f8cc1905
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2020-09-05 10:46:23 +0900

    Add files via upload

---
+++ project17/springbootactuatordemo/src/test/java/myepark/ebaykorea/springbootactuatordemo/SpringbootactuatordemoApplicationTests.java
@@ -0,0 +1,13 @@
+package myepark.ebaykorea.springbootactuatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootactuatordemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
